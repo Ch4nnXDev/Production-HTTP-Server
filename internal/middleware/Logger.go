edb4d82963e796837f64d90c8b8ae6b323294232
Channa2146@gmail.com <Channa2146@gmail.com>
2026-09-16 09:57:07 +0530

    added Logger code

--- internal/middleware/Logger.go
+++ internal/middleware/Logger.go
@@ -3,17 +3,53 @@ package middleware
 import (
 	"net/http"
 	"log"
+	"time"
 )
 
-
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+	
+}
+//* = means this points to the type of this variable
+//& = means this points to the variable address
 
 func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 
-		log.Println(req.Method, req.URL.Path)
+		start := time.Now()
 
-		next.ServeHTTP(w, req)
+		rw := &responseWriter{
+			ResponseWriter: w,
+		}
+
+		next.ServeHTTP(rw, req)
+
+		requestID := req.Context().Value(requestIDKey)
+		
+
+		log.Println(requestID, req.Method, req.URL.Path, rw.statusCode, time.Since(start), req.RemoteAddr)
+
+		
 
 	})
 
+}
+
+func (rw *responseWriter) WriteHeader(statusCode int) {
+
+	if rw.statusCode != 0 {
+		return
+	}
+	rw.ResponseWriter.WriteHeader(statusCode)
+}
+
+
+func (rw *responseWriter) Write(body []byte) (int, error) {
+
+    if rw.statusCode == 0 {
+        rw.WriteHeader(http.StatusOK)
+    }
+
+    return rw.ResponseWriter.Write(body)
 }
